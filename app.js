2ce47106c7c57ac9a4743b14d38f148cc563bf39
BisalBosu <bishalbosu@gmail.com>
2023-04-07 18:37:22 +0530

    add cart and delete cart implemented

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const errorController = require("./controllers/error")
 const sequelize = require("./util/database")
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 
 const app = express()
 
@@ -15,6 +18,8 @@ app.set("views", "views")
 
 const adminRoutes = require("./routes/admin")
 const shopRoutes = require("./routes/shop")
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
@@ -36,7 +41,11 @@ app.use(errorController.get404)
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
 
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
 
 
 sequelize
@@ -54,7 +63,11 @@ sequelize
 	})
 	.then((user) => {
 		// console.log(user);
-		app.listen(3000)
+		return user.createCart();
+		
+	})
+	.then(cart =>{
+		app.listen(3000);
 	})
 	.catch((err) => {
 		console.log(err)
